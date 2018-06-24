7a1224e8cb1545c18bb543266f157dda09f96942
daolao <dalao1002@gmail.com>
2018-06-24 10:16:08 +0200

    build jar

---
+++ src/main/java/angular/UI.java
@@ -0,0 +1,12 @@
+package angular;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UI {
+    public static void main(String[] args) {
+        SpringApplication.run(UI.class, args);
+    }
+
+}
