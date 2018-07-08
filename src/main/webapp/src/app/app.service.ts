cb7114592831b2c7e43bfafd221f1b05091d7d9b
daolao <dalao1002@gmail.com>
2018-07-08 15:02:42 +0200

    cache languages

--- src/main/webapp/src/app/app.service.ts
+++ src/main/webapp/src/app/app.service.ts
@@ -3,14 +3,21 @@ import {HttpClient} from "@angular/common/http";
 
 @Injectable()
 export class AppService {
+  languages: Map<string, Object> = new Map<string, Object>();
+
   constructor(private http: HttpClient) {
 
   }
 
-  switchLanguage(lan: string, callback) {
-    this.http.get("?language=" + lan, {headers: {"X-Requested-With": "XMLHttpRequest"}}).subscribe(data => {
-        return callback && callback(data);
-      }
-    );
+  switchLanguage(language: string, callback) {
+    if (this.languages.get(language) == null) {
+      this.http.get("?language=" + language, {headers: {"X-Requested-With": "XMLHttpRequest"}}).subscribe(data => {
+          this.languages.set(language, data);
+          return callback && callback(data);
+        }
+      );
+    } else {
+      return callback && callback(this.languages.get(language));
+    }
   }
 }
