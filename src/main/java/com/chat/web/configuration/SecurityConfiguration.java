c47db9ce9abe1a0cf2cf3f20836c95492e4a6735
daolao <dalao1002@gmail.com>
2018-07-08 22:05:40 +0200

    require ssl

---
+++ src/main/java/com/chat/web/configuration/SecurityConfiguration.java
@@ -0,0 +1,16 @@
+package com.chat.web.configuration;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@Order(SecurityProperties.IGNORED_ORDER)
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.requiresChannel().anyRequest().requiresSecure();
+    }
+}
