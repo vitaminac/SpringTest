be54f55be37df09e1c9f938cf8fece347a1177b6
daolao <dalao1002@gmail.com>
2018-07-15 23:12:24 +0200

    Support both https and http && require authentication

--- src/main/java/com/chat/web/configuration/PathMappingConstants.java
+++ src/main/java/com/chat/web/configuration/PathMappingConstants.java
@@ -4,5 +4,14 @@ public class PathMappingConstants {
     public static final String MatchesAllPath = "/{path:[^\\.]+}/**";
     public static final String ForwardToRootPath = "forward:/";
     public static final String UserMappingPath = "/users";
+    public static final String LoginMappingPath = "/login";
     public static final String LanguageMappingPath = "/languages/{language}";
+
+    // filter from authentication
+    public static final String RootMappingPath = "/";
+    public static final String IndexMappingPath = "/index.html";
+    public static final String FaviconIconMappingPath = "/favicon.ico";
+    public static final String FilterAllJavaScript = "/*.js";
+    public static final String FilterAllStyleSheet = "/*.css";
+    public static final String FilterLanguagesMappingPath = "/languages/*";
 }
--- src/main/java/com/chat/web/configuration/SecurityConfiguration.java
+++ src/main/java/com/chat/web/configuration/SecurityConfiguration.java
@@ -3,8 +3,17 @@ package com.chat.web.configuration;
 import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import static com.chat.web.configuration.PathMappingConstants.FaviconIconMappingPath;
+import static com.chat.web.configuration.PathMappingConstants.FilterAllJavaScript;
+import static com.chat.web.configuration.PathMappingConstants.FilterAllStyleSheet;
+import static com.chat.web.configuration.PathMappingConstants.FilterLanguagesMappingPath;
+import static com.chat.web.configuration.PathMappingConstants.IndexMappingPath;
+import static com.chat.web.configuration.PathMappingConstants.LoginMappingPath;
+import static com.chat.web.configuration.PathMappingConstants.RootMappingPath;
 
 @Configuration
 @Order(SecurityProperties.IGNORED_ORDER)
@@ -12,5 +21,10 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.requiresChannel().anyRequest().requiresSecure();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, RootMappingPath, IndexMappingPath, FilterAllJavaScript, FilterAllStyleSheet, FaviconIconMappingPath, FilterLanguagesMappingPath)
+                .permitAll();
+        http.authorizeRequests().antMatchers(LoginMappingPath).permitAll();
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
