0193fb71d8d0a1700b019769ec6c99a5a604d317
daolao <dalao1002@gmail.com>
2018-08-19 17:54:28 +0200

    remove language support config bean

--- src/main/java/com/core/web/config/AppConfig.java
+++ src/main/java/com/core/web/config/AppConfig.java
@@ -1,24 +1,7 @@
 package com.core.web.config;
 
-import com.core.web.service.Traductor;
-import com.core.web.model.language.English;
-import com.core.web.model.language.Language;
-import com.core.web.model.language.Spanish;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @Configuration
 public class AppConfig {
-    @Bean
-    public Traductor traductor() {
-        Map<String, Language> languages = new HashMap<>();
-        Language defaultLanguage = new English();
-        languages.put(defaultLanguage.getId(), defaultLanguage);
-        Language language = new Spanish();
-        languages.put(language.getId(), language);
-        return new Traductor(languages, defaultLanguage);
-    }
 }
